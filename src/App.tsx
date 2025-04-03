6144c1954d3387b1948a682591ef36dff7cf9399
j0n0m2 <dbsk8223@naver.com>
2025-04-03 16:50:34 +0900

    dev 충돌 해결

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,12 @@ import router from './router'
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="flex justify-center min-h-screen bg-white">
-      <div className="w-[480px]">
-        <Login />
-      </div>
-    </div>
-=======
     <RouterProvider
       router={router}
       future={{
         v7_startTransition: true,
       }}
     />
->>>>>>> e62cc37e7610c759200e6cd6e96ce7ad34d0d7c6
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-import App from './App.tsx'
-import './shared/model/styles/globals.css'
-import { createRoot } from 'react-dom/client'
-=======
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import './shared/model/styles/globals.css'
->>>>>>> e62cc37e7610c759200e6cd6e96ce7ad34d0d7c6
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
