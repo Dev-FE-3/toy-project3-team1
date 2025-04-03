2f414e70da1a37741cb1c93900bec17d7e970296
j0n0m2 <dbsk8223@naver.com>
2025-04-03 21:41:55 +0900

    Feat : 검색, 게임 카테고리, 카테고리 추가 버튼 구현

---
+++ src/pages/Home/constants.ts
@@ -0,0 +1,8 @@
+export const userFavoriteGames = {
+  overWatch: 'src/assets/images/Overwatch_logo.png',
+  LoL: 'src/assets/images/lol-logo.png',
+  // 예시
+  // valorant: 'src/assets/images/valorant-logo.png',
+  // csgo: 'src/assets/images/csgo-logo.png',
+  // apex: 'src/assets/images/apex-logo.png',
+}
