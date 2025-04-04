6d4a2eb88a161c4d53c8170b1d950df20c4dc81b
j0n0m2 <dbsk8223@naver.com>
2025-04-04 16:30:06 +0900

    Fix : 서비스 로고 이미지 추가 및 layout 구조 수정

--- src/shared/components/layout/Layout.tsx
+++ src/shared/components/layout/Layout.tsx
@@ -2,37 +2,47 @@ import { Outlet } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { House, SquarePlus, ListVideo, UserRound } from 'lucide-react'
 
+const Header = () => {
+  return (
+    <header className="border-c800 flex h-[66px] w-[480px] items-center border-b pl-5">
+      <img src="/images/logo.svg"></img>
+    </header>
+  )
+}
+
+const NavBar = () => {
+  return (
+    <nav className="border-c800 flex justify-around border-t py-4">
+      <Link to="/" className="text-white">
+        <House />
+      </Link>
+      <Link to="/create" className="text-white">
+        <SquarePlus />
+      </Link>
+      <Link to="/feed" className="text-white">
+        <ListVideo />
+      </Link>
+      <Link to="/profile" className="text-white">
+        <UserRound
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        />
+      </Link>
+    </nav>
+  )
+}
+
 const Layout = () => {
   return (
-    <div className="relative min-h-screen bg-slate-900 p-4 pb-16 text-white">
-      <header>
-        <h1 className="text-h2 mb-4 font-bold">리플레이</h1>
-      </header>
-
-      <main>
+    <div className="">
+      <Header />
+      <main className="h-[calc(100vh-66px-66px)] px-[20px]">
         <Outlet />
       </main>
-
-      <nav className="absolute right-0 bottom-0 left-0 flex justify-around border-t border-slate-800 bg-slate-900 py-4">
-        <Link to="/" className="text-white">
-          <House />
-        </Link>
-        <Link to="/create" className="text-white">
-          <SquarePlus />
-        </Link>
-        <Link to="/feed" className="text-white">
-          <ListVideo />
-        </Link>
-        <Link to="/profile" className="text-white">
-          <UserRound
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          />
-        </Link>
-      </nav>
+      <NavBar />
     </div>
   )
 }
