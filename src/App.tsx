d4174c3f2f4f01d4e007c42afd6c3efd49a1c920
Jungkyu Yang <didwjd8590412@gmail.com>
2025-04-05 12:45:10 +0900

    chore: React Query Devtools 설치 및 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { RouterProvider } from 'react-router-dom'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import router from './router'
 
 function App() {
   return (
-    <RouterProvider
-      router={router}
-      future={{
-        v7_startTransition: true,
-      }}
-    />
+    <>
+      <RouterProvider
+        router={router}
+        future={{
+          v7_startTransition: true,
+        }}
+      />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
   )
 }
 
