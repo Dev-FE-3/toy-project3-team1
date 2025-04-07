139041c5b389239960366c07a528a393258d03d9
j0n0m2 <dbsk8223@naver.com>
2025-04-07 13:42:30 +0900

    Fix : BackButton props 수정

--- src/shared/components/BackButton/BackButton.tsx
+++ src/shared/components/BackButton/BackButton.tsx
@@ -1,9 +1,5 @@
 import { ArrowLeft } from 'lucide-react'
 
-interface BackButtonProps {
-  color?: string
-}
-
-export default function BackButton({ color = 'text-50' }: BackButtonProps) {
-  return <ArrowLeft size={24} className={color} />
+export default function BackButton() {
+  return <ArrowLeft size={24} className='text-c400' />
 }
