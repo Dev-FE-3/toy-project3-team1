a46fe944836db42b4caf2655bae79f8ff3e581cf
j0n0m2 <dbsk8223@naver.com>
2025-04-08 12:25:52 +0900

    Fix : layout padding 수정

--- src/shared/components/layout/Layout.tsx
+++ src/shared/components/layout/Layout.tsx
@@ -14,7 +14,7 @@ const Layout = () => {
     <>
       <main className="relative h-screen ">
         <Header />
-        <div className="no-scrollbar h-[calc(100vh-66px-66px)] overflow-x-clip overflow-y-scroll px-[20px]">
+        <div className="no-scrollbar h-[calc(100vh-66px-66px)] overflow-x-clip overflow-y-scroll">
           <Outlet />
         </div>
         <BottomNav />
