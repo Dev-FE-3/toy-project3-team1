3a3ee73884f8445e2ae7a52014541184dcb43659
j0n0m2 <dbsk8223@naver.com>
2025-04-09 19:06:08 +0900

    Feat : Login 페이지 리플레이 텍스트 로고 추가

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -29,7 +29,10 @@ export default function LoginPage() {
   return (
     <div className="bg-c900 flex min-h-screen flex-col">
       <div className="mx-auto flex w-full max-w-[390px] flex-1 flex-col p-3">
-        <h1 className="text-c50 text-h2 mt-[200px] mb-[100px] text-center">리플레이</h1>
+        <img
+          src="/images/logo.svg"
+          className="text-c50 text-h2 m-auto mt-[200px] mb-[100px] w-[200px]"
+        />
 
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
