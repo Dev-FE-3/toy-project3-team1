9543682cbe701c3fed4d5c9a400303d378d1382c
j0n0m2 <dbsk8223@naver.com>
2025-04-10 19:07:02 +0900

    Categories 주석 제거

--- src/features/Home/components/Categories/Categories.tsx
+++ src/features/Home/components/Categories/Categories.tsx
@@ -5,13 +5,11 @@ import { games } from '../../constants'
 
 export const Categories = ({ count, onCategorySelect, selectedCategory }: CategoriesProps) => {
   const handleCategoryClick = (gameName: string) => {
-    // 이제 null을 전달해도 타입 에러가 발생하지 않습니다
     onCategorySelect(gameName === selectedCategory ? null : gameName)
   }
 
   return (
     <div className="bg-c900 flex h-[98px] w-[480px] items-center justify-center gap-4">
-      {/* null을 전달해도 타입 에러가 발생하지 않습니다 */}
       <Category>
         <Search size={34} strokeWidth={1.2} className="text-c200" />
       </Category>
