1a3697e1caebb42d84aaee583891d5d6b7d828cb
최정훈 <chjjh0gmail.com>
2025-04-11 16:22:57 +0900

    hotfix: github 충돌 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import { RouterProvider } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-<<<<<<< Updated upstream
-import router from './router'
-
-const queryClient = new QueryClient()
-=======
 import router from './router/router'
 import { AuthProvider } from './shared/model/contexts/AuthContext'
 import { queryClient } from './shared/model/lib/queryClient'
->>>>>>> Stashed changes
 
 function App() {
   return (
