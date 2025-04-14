d847915c99a3af075c845678898fc0e3e107f338
j0n0m2 <dbsk8223@naver.com>
2025-04-15 00:32:50 +0900

    Refator : useInView를 활용하여 무한 스크롤 감지 요소 생성

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -4,9 +4,10 @@ import { PlaylistContainer } from '@/pages/Home/components/PlaylistContainer'
 import { GAMES } from './constants/GAMES'
 import { limitCategoryCount } from './utils/limitCategoryCount'
 import { useInfinitePlaylists } from './hooks/useInfinitePlaylists'
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import { useGetAuthState } from '@/shared/model/contexts/AuthContext'
 import { queryClient } from '@/shared/model/lib/queryClient'
+import { useInView } from 'react-intersection-observer'
 
 const HomePage = () => {
   const { profile } = useGetAuthState()
@@ -15,7 +16,15 @@ const HomePage = () => {
     profile?.id,
     selectedCategory,
   ) // 무한 스크롤을 위한 데이터 가져오기 (로그인한 사용자의 플레이리스트 제외)
+  const { ref: loadMoreRef, inView } = useInView({ threshold: 1 }) // 무한 스크롤을 위한 감지 요소
   const gameCount = limitCategoryCount(GAMES.length) // 카테고리 개수 제한
+
+  // inview가 true일 때 fetchNextPage 호출
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage()
+    }
+  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage])
 
   // 사용자 변경 시 쿼리 초기화
   useEffect(() => {
@@ -27,19 +36,6 @@ const HomePage = () => {
   }, [profile?.id])
 
   const playlists = data?.pages.flat() ?? []
-  const loadMoreRef = useRef(null)
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting && hasNextPage && !isFetchingNextPage) {
-          fetchNextPage()
-        }
-      },
-      { threshold: 1.0 },
-    )
-    if (loadMoreRef.current) observer.observe(loadMoreRef.current)
-    return () => observer.disconnect()
-  }, [hasNextPage, isFetchingNextPage])
 
   return (
     <>
@@ -50,7 +46,11 @@ const HomePage = () => {
         selectedCategory={selectedCategory}
       />
       <PlaylistContainer key={selectedCategory} playlists={playlists} />
-      <div ref={loadMoreRef} />
+      {/* 감시 대상 엘리먼트 */}
+      <div ref={loadMoreRef} style={{ height: 1 }} />
+
+      {/* 로딩 중이면 로딩 스피너 보여주기 */}
+      {isFetchingNextPage && <div style={{ textAlign: 'center' }}>불러오는 중...</div>}
     </>
   )
 }
