37c9d810eb324eb8e0ddab866f9c515f53956953
j0n0m2 <dbsk8223@naver.com>
2025-04-14 18:03:36 +0900

    Fix : profile 메뉴 로직 Link로 변경

--- src/shared/components/BottomNav/BottomNav.tsx
+++ src/shared/components/BottomNav/BottomNav.tsx
@@ -10,22 +10,16 @@ export default function BottomNav() {
       <Link to={'/playlist/new'} className="flex h-full w-[88px] items-center justify-center">
         <SquarePlus size={24} className="text-c50" />
       </Link>
-      {/* <Link to={'/feed'} className="flex h-full w-[88px] items-center justify-center">
-        <ListVideo size={24} className="text-c50" />
-      </Link> */}
-      {/* <Link to={'/profile'} className="flex h-full w-[88px] items-center justify-center">
-        <UserRound size={24} className="text-c50" />
-      </Link> */}
-      <Link
-        to={'/playlist/d276b4f1-d2bf-4325-baab-7ee0dbc314c2'}
-        className="flex h-full w-[88px] items-center justify-center"
-      >
+      <Link to={'/playlists'} className="flex h-full w-[88px] items-center justify-center">
         <ListVideo size={24} className="text-c50" />
       </Link>
-      {/* 임시 로그아웃 */}
-      <div className="flex h-full w-[88px] items-center justify-center" onClick={() => signOut()}>
+      <Link to={'/profile'} className="flex h-full w-[88px] items-center justify-center">
         <UserRound size={24} className="text-c50" />
-      </div>
+      </Link>
+      {/* 임시 로그아웃 */}
+      {/* <div className="flex h-full w-[88px] items-center justify-center" onClick={() => signOut()}>
+        <UserRound size={24} className="text-c50" />
+      </div> */}
     </nav>
   )
 }
