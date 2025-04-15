9a66a7f9dc3c5d46adafd34bc3ec91d4494b730f
j0n0m2 <dbsk8223@naver.com>
2025-04-16 01:04:20 +0900

    Refactor : 홈페이지 스켈레톤UI 코드에 주석 추가

--- src/pages/Home/components/HomePageSkeleton.tsx
+++ src/pages/Home/components/HomePageSkeleton.tsx
@@ -2,22 +2,25 @@ import { Skeleton } from '@/shared/components/ui/skeleton'
 
 const HomePageSkeleton = () => {
   return (
-    <div className="space-y-4">
+    <>
       {/* 플레이리스트 카드 스켈레톤 */}
-      <div className="relative">
+      <div className="relative space-y-4">
+        {/* 포커스된 플레이리스트 */}
         <div className="absolute top-12 z-10 h-full w-full">
           <div className="bg-c700 w-full space-y-5 p-[30px]">
-            <Skeleton className="aspect-video rounded-xl" />
-            <Skeleton className="h-9 w-3/4" />
-            <Skeleton className="h-7 w-1/2" />
+            <Skeleton className="aspect-video rounded-xl" /> {/* 플레이리스트 썸네일 */}
+            <Skeleton className="h-9 w-3/4" /> {/* 플레이리스트 제목 */}
+            <Skeleton className="h-7 w-1/2" /> {/* 플레이리스트 게시자 */}
           </div>
         </div>
+        {/* 다음 플레이리스트 카드 */}
         <div className="absolute top-80 h-full w-full space-y-5 p-[30px]">
-          <Skeleton className="bg-c800 aspect-video w-full rounded-xl" />
-          <Skeleton className="bg-c800 h-9 w-3/4" />
+          <Skeleton className="bg-c800 aspect-video w-full rounded-xl" />{' '}
+          {/* 플레이리스트 썸네일 */}
+          <Skeleton className="bg-c800 h-9 w-3/4" /> {/* 플레이리스트 제목 */}
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
