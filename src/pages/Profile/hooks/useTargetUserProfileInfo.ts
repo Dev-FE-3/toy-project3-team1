4fe932da3448bb765c466f555a3c8c9cadb5054c
j0n0m2 <dbsk8223@naver.com>
2025-04-16 00:42:27 +0900

    Refactor : Error 처리 추가

--- src/pages/Profile/hooks/useTargetUserProfileInfo.ts
+++ src/pages/Profile/hooks/useTargetUserProfileInfo.ts
@@ -5,11 +5,12 @@ export const useTargetUserProfileInfo = (targetProfileId?: string) => {
   return useQuery({
     queryKey: ['profile', targetProfileId],
     queryFn: async () => {
-      const { data } = await supabase
+      const { data, error } = await supabase
         .from('profiles')
         .select('*')
         .eq('id', targetProfileId)
         .maybeSingle()
+      if (error) throw new Error('Error fetching target user profile info')
       return data
     },
     enabled: !!targetProfileId, // null이면 쿼리 안 함
