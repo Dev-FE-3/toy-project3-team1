24e673f98c3236275f5bb1d775d669e66f64a60a
j0n0m2 <dbsk8223@naver.com>
2025-04-16 23:25:48 +0900

    Style : layout width 수정

--- src/shared/components/layout/Layout.tsx
+++ src/shared/components/layout/Layout.tsx
@@ -12,7 +12,7 @@ const Header = () => {
 
 const Layout = () => {
   return (
-    <div className="relative flex min-h-screen flex-col">
+    <div className="relative flex h-screen flex-col">
       <main id="main" className="flex-1">
         <Header />
         <div className="no-scrollbar h-[calc(100vh-66px-66px)] overflow-x-clip">
