40999df4c57372e181e4487ac795ebeb39019a51
Jungkyu Yang <didwjd8590412@gmail.com>
2025-04-17 18:00:17 +0900

    fix: 로고 경로 및 파일 포맷변경

--- src/pages/Home/constants/GAMES.ts
+++ src/pages/Home/constants/GAMES.ts
@@ -1,3 +1,7 @@
+import LolLogo from '@/assets/images/lol_logo.webp'
+import WhiteOverwatchLogo from '@/assets/images/Overwatch_logo_white.webp'
+import ValorantLogo from '@/assets/images/Valorant_logo_name.webp'
+
 export interface Game {
   id: string
   name: string
@@ -8,21 +12,21 @@ export const GAMES: Game[] = [
   {
     id: 'overwatch',
     name: '오버워치',
-    logoUrl: 'src/assets/images/Overwatch_logo.png',
+    logoUrl: WhiteOverwatchLogo,
   },
   {
     id: 'lol',
     name: '롤',
-    logoUrl: 'src/assets/images/lol-logo.png',
+    logoUrl: LolLogo,
   },
   {
     id: 'valorant',
     name: '발로란트',
-    logoUrl: 'src/assets/images/valorant-logo.png',
+    logoUrl: ValorantLogo,
   },
-  {
-    id: 'battleground',
-    name: '배틀그라운드',
-    logoUrl: 'src/assets/images/csgo-logo.png',
-  },
+  // {
+  //   id: 'battleground',
+  //   name: '배틀그라운드',
+  //   logoUrl: CsLogo,
+  // },
 ] as const
