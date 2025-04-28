80186a0cf84436485e31eb2e16629163b5897c75
Jungkyu Yang <didwjd8590412@gmail.com>
2025-04-28 23:34:16 +0900

    feat: 공용 토스트 메시지 유틸 추가 및 전역 적용

--- src/shared/model/lib/queryClient.ts
+++ src/shared/model/lib/queryClient.ts
@@ -1,7 +1,30 @@
-import { QueryClient } from '@tanstack/react-query'
-
+import { mapErrorMessage } from '@/shared/model/utils/mapErrorMessage'
+import { toast } from '@/shared/model/utils/toast'
+import { MutationCache, QueryClient } from '@tanstack/react-query'
 // QueryClient 기본 설정
 export const queryClient = new QueryClient({
+  // 모든 useMutation 공통 후킹 지점
+  mutationCache: new MutationCache({
+    // 실패한 경우: meta.toast 플래그가 있을 때만 알림
+    onError: (error, _vars, _ctx, mutation) => {
+      if (mutation.meta?.toastError) {
+        const rawMsg = mutation.meta.toastErrorMessage
+        const fallbackMsg = mapErrorMessage(error)
+        const customMsg = typeof rawMsg === 'string' || Array.isArray(rawMsg) ? rawMsg : fallbackMsg
+
+        toast.error(customMsg)
+      }
+    },
+
+    // 성공 알림 예시 (옵션)
+    onSuccess: (_data, _vars, _ctx, mutation) => {
+      if (mutation.meta?.toastSuccess) {
+        const rawMsg = mutation.meta.toastSuccessMessage
+        const customMsg = typeof rawMsg === 'string' ? rawMsg : '처리가 완료되었습니다!'
+        toast.success(customMsg)
+      }
+    },
+  }),
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false, // 윈도우 포커스시 자동으로 데이터를 다시 가져오지 않음
